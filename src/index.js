b8218eb4d661cd53b55545266fc16ade23b66f9a
Thies Nieborg <thies2003@gmail.com>
2021-02-14 10:35:21 +0000

    Added auto restarting when listening to a new channel

--- src/index.js
+++ src/index.js
@@ -5,142 +5,165 @@ dotenv.config();
 const MongoClient = require("mongodb").MongoClient;
 
 async function main() {
-    const db = (
-        await MongoClient.connect("mongodb://db:27017", {
-            useUnifiedTopology: true,
+    return new Promise(async (done, err) => {
+        const db = (
+            await MongoClient.connect("mongodb://db:27017", {
+                useUnifiedTopology: true,
+            })
+        ).db("testing");
+        const listening = await (await db.collection("listening").find({}).toArray()).map(x => {
+            return x.name
         })
-    ).db("testing");
-    const listening = await db.collection("listening").find({}).toArray();
 
-    const client = new tmi.Client({
-        identity: {
-            username: process.env.TMI_USER,
-            password: process.env.TMI_KEY,
-        },
-        connection: {
-            reconnect: true,
-            secure: true,
-        },
-        channels: [...listening, process.env.TMI_USER],
-    });
+        const client = new tmi.Client({
+            identity: {
+                username: process.env.TMI_USER,
+                password: process.env.TMI_KEY,
+            },
+            connection: {
+                reconnect: true,
+                secure: true,
+            },
+            channels: [...listening, process.env.TMI_USER],
+        });
 
-    const usersColl = db.collection("users");
+        const usersColl = db.collection("users");
 
-    const triggersColl = db.collection("triggers");
-    const reactionsColl = db.collection("reactions");
+        const triggersColl = db.collection("triggers");
+        const reactionsColl = db.collection("reactions");
 
-    const commandsColl = db.collection("commands");
+        const commandsColl = db.collection("commands");
 
-    await client.connect();
-    console.log("Goewan bot initialized");
+        await client.connect();
+        console.log("Goewan bot initialized");
 
-    let users;
-    let triggers;
-    const refreshUsers = async () => {
-        users = await usersColl.find({}).toArray();
-    };
-    const refreshTriggers = async () => {
-        triggers = await triggersColl.find({}).toArray();
-    };
+        let users;
+        let triggers;
+        const refreshUsers = async () => {
+            users = await usersColl.find({}).toArray();
+        };
+        const refreshTriggers = async () => {
+            triggers = await triggersColl.find({}).toArray();
+        };
 
-    await refreshUsers();
-    await refreshTriggers();
+        await refreshUsers();
+        await refreshTriggers();
 
-    client.on("message", async (channel, tags, message, self) => {
-        const { username } = tags;
-        const sendMsg = (msg) => client.say(channel, `@${username}, ${msg}`);
-        if (self) return;
+        client.on("message", async (channel, tags, message, self) => {
+            const { username } = tags;
+            const sendMsg = (msg) => client.say(channel, `@${username}, ${msg}`);
+            if (self) return;
 
-        const tempCommand = message.match(/\%\w+|\w+|"[^"]+"/g).map(x => x.replace(/\"/g, ""))
-        const command = tempCommand.shift().slice(1);
+            const tempCommand = message.match(/\%\w+|\w+|"[^"]+"/g).map(x => x.replace(/\"/g, ""))
+            const command = tempCommand.shift().slice(1);
 
-        const firstArg = tempCommand.shift();
-        const args = tempCommand.join(" ");
+            const firstArg = tempCommand.shift();
+            const args = tempCommand.join(" ");
 
-        console.log("Message command:", command, "with firstarg:", firstArg, "and the rest of the args:", args)
+            console.log("Message command:", command, "with firstarg:", firstArg, "and the rest of the args:", args)
 
-        if (username === process.env.ADMIN_USER && message[0] === "%") {
-            switch (command) {
-                case "dump":
-                    let res = await exec(`mongodump --uri="mongodb://db:27017"`)
-                    console.log(res)
-                    return;
-                case "restore":
-                    let res2 = await exec(`mongorestore --uri="mongodb://db:27017" dump/`)
-                    console.log(res2)
-                    await refreshUsers();
-                    await refreshTriggers();
-                    return;
+            if (username === process.env.ADMIN_USER && message[0] === "%") {
+                switch (command) {
+                    case "listen":
+                        await update(db.collection("listening"), firstArg)
+                        client.disconnect();
+                        client.removeAllListeners("message");
+                        err("RECON")
+                        break;
+                    case "unlisten":
+                        await del(db.collection("listening"), firstArg)
+                        client.disconnect();
+                        client.removeAllListeners("message");
+                        err("RECON")
+                        break;
 
-                case "editreaction":
-                case "addreaction":
-                    await update(reactionsColl, firstArg, args)
-                    return;
-                case "delreaction":
-                    await del(reactionsColl, firstArg)
-                    return;
+                    case "dump":
+                        let res = await exec(`mongodump --uri="mongodb://db:27017"`)
+                        console.log(res)
+                        return;
+                    case "restore":
+                        let res2 = await exec(`mongorestore --uri="mongodb://db:27017" dump/`)
+                        console.log(res2)
+                        await refreshUsers();
+                        await refreshTriggers();
+                        return;
 
-                case "edittrigger":
-                case "addtrigger":
-                    await update(triggersColl, firstArg, args.length === 0 ? firstArg : args)
-                    await refreshTriggers();
-                    return;
-                case "deltrigger":
-                    await del(triggersColl, firstArg)
-                    await refreshTriggers();
-                    return;
+                    case "editreaction":
+                    case "addreaction":
+                        await update(reactionsColl, firstArg, args)
+                        return;
+                    case "delreaction":
+                        await del(reactionsColl, firstArg)
+                        return;
 
-                case "editcmd":
-                case "addcmd":
-                    await update(commandsColl, firstArg, args)
-                    return;
-                case "delcmd":
-                    await del(commandsColl, firstArg)
-                    return;
+                    case "edittrigger":
+                    case "addtrigger":
+                        await update(triggersColl, firstArg, args.length === 0 ? firstArg : args)
+                        await refreshTriggers();
+                        return;
+                    case "deltrigger":
+                        await del(triggersColl, firstArg)
+                        await refreshTriggers();
+                        return;
 
-                case "trust":
-                    await update(usersColl, firstArg)
-                    await refreshUsers();
-                    return;
-                case "untrust":
-                    await del(usersColl, firstArg)
-                    await refreshUsers();
-                    return;
-            }
-        }
+                    case "editcmd":
+                    case "addcmd":
+                        await update(commandsColl, firstArg, args)
+                        return;
+                    case "delcmd":
+                        await del(commandsColl, firstArg)
+                        return;
 
-        if (users.find((x) => x.name === username)) {
-            if (message[0] === "%") {
-                const found = await commandsColl.findOne({
-                    name: command,
-                });
-                if (!found) return;
-                sendMsg(found.response);
-            } else {
-                const triggerFound = triggers.find(x =>
-                    message.includes(x.name)
-                )
-
-                if (triggerFound && triggerFound.response) {
-                    sendMsg(triggerFound.response)
-                } else if (triggerFound) {
-                    const random = await reactionsColl.aggregate(
-                        [{ $sample: { size: 1 } }]
-                    ).next()
-                    sendMsg(random.response)
+                    case "trust":
+                        await update(usersColl, firstArg)
+                        await refreshUsers();
+                        return;
+                    case "untrust":
+                        await del(usersColl, firstArg)
+                        await refreshUsers();
+                        return;
                 }
-                return
             }
-        }
-    });
+
+            if (users.find((x) => x.name === username)) {
+                if (message[0] === "%") {
+                    const found = await commandsColl.findOne({
+                        name: command,
+                    });
+                    if (!found) return;
+                    sendMsg(found.response);
+                } else {
+                    const triggerFound = triggers.find(x =>
+                        message.includes(x.name)
+                    )
+
+                    if (triggerFound && triggerFound.response) {
+                        sendMsg(triggerFound.response)
+                    } else if (triggerFound) {
+                        const random = await reactionsColl.aggregate(
+                            [{ $sample: { size: 1 } }]
+                        ).next()
+                        sendMsg(random.response)
+                    }
+                    return
+                }
+            }
+        });
+    })
 }
 
 (async () => {
-    try {
-        await main();
-    } catch (e) {
-        console.error(e);
-        process.exit(1);
+    let error = null;
+    while (!error) {
+        try {
+            await main();
+        } catch (e) {
+            console.error("CHECKING: ", e)
+            if (e !== "RECON") {
+                console.error(e);
+                process.exit(1);
+            }
+        }
     }
 })();
 
